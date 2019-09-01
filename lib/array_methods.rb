b5676828ca0c49348fcf56f8f4dc41e1dd73f6ed
Laina Karosic <lainakarosic@gmail.com>
2019-09-01 16:12:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  array.length.times do |value_to_find|
-  puts array[value_to_find]
+  array.length.times do { |value_to_find|
+    puts array[value_to_find]
+  }
 end
 
 
