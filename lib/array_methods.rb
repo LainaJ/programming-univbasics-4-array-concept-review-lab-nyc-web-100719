7127a05c07fd36023d1ce747a35feb6e8cd88be3
Laina Karosic <lainakarosic@gmail.com>
2019-09-01 17:21:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,5 +19,14 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = 0
+
+  array.length.times do |count|
+    if array[count] < min
+      min = array[count]
+    end
+  end
+ return min
+end
+
 end
