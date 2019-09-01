9256ef60cb7ea7c5a46d9685ea1f4fa5b0a0ed53
Laina Karosic <lainakarosic@gmail.com>
2019-09-01 16:16:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  array.length.times { |index|
-    puts array[index]
+  array.length.times { |value_to_find|
+    puts array[value_to_find]
   }
 end
 
